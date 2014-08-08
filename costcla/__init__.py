29745315b034566a20f4c451c6dc6ea377b27154
Al.Bahnsen <al.bahnsen@gmail.com>
2014-08-08 11:06:19 +0200

    main init file

---
+++ costcla/__init__.py
@@ -0,0 +1,17 @@
+"""
+Cost-Sensitive Supervised Learning module for Python
+==================================
+costcla is a Python module that include state-of-the-art
+Cost-Sensitive Supervised Learning algorithms, built on top of
+scikit-learn, numpy and scipy.
+See #Fill# for complete documentation.
+"""
+
+# Authors: Alejandro Correa Bahnsen <al.bahnsen@gmail.com>
+# License: BSD 3 clause
+
+__version__ = '0.02-git'
+
+from metrics import *
+from datasets import *
+from models import *
