3c190b9f4b463e05013fbbb4eaa3f0b46e50cc16
Al.Bahnsen <al.bahnsen@gmail.com>
2014-08-09 21:11:18 +0200

    test with bagging

--- costcla/models/cost_ensemble.py
+++ costcla/models/cost_ensemble.py
@@ -1,93 +1,92 @@
 __author__ = 'al'
 
 
-from sklearn.ensemble import BaggingClassifier
-
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.cross_validation import train_test_split
-from costcla.datasets import load_creditscoring1
+from costcla.datasets import load_creditscoring2
 from costcla.models import CostSensitiveDecisionTreeClassifier
 from costcla.metrics import savings_score
+import numpy as np
+from costcla.models.bagging import BaggingClassifier
 
-data = load_creditscoring1()
+data = load_creditscoring2()
 sets = train_test_split(data.data, data.target, data.cost_mat, test_size=0.33, random_state=0)
 
 X_train, X_test, y_train, y_test, cost_mat_train, cost_mat_test = sets
 
-
-X = X_train
-y = y_train
-cost_mat = cost_mat_train
-
-
-import numpy as np
-
-
-from costcla.models.bagging import BaggingClassifier
-
-f = BaggingClassifier(base_estimator=CostSensitiveDecisionTreeClassifier(),
-                      n_estimators=3, max_samples=0.2, max_features=0.5, verbose=2,
-                      bootstrap=False, bootstrap_features=False, n_jobs=1)
-
-f.fit(X, y, cost_mat)
-
-y_pred_test_csdt_b = f.predict_proba(X_test)
+# f = BaggingClassifier(base_estimator=CostSensitiveDecisionTreeClassifier(),
+#                       n_estimators=3, max_samples=0.2, max_features=0.5, verbose=2,
+#                       bootstrap=False, bootstrap_features=False, n_jobs=1)
 
 
 
-n_classes = 2
+class BaggingCostSensitiveDecisionTreeClassifier(BaggingClassifier):
+    def __init__(self,
+                 n_estimators=100,
+                 max_samples=0.5,
+                 verbose=False):
+        super(BaggingClassifier, self).__init__(
+            base_estimator=CostSensitiveDecisionTreeClassifier(),
+            n_estimators=n_estimators,
+            max_samples=max_samples,
+            max_features=1.0,
+            bootstrap=True,
+            bootstrap_features=False,
+            n_jobs=1,
+            random_state=None,
+            verbose=verbose)
+
+class PastingCostSensitiveDecisionTreeClassifier(BaggingClassifier):
+    def __init__(self,
+                 n_estimators=100,
+                 max_samples=0.5,
+                 verbose=False):
+        super(BaggingClassifier, self).__init__(
+            base_estimator=CostSensitiveDecisionTreeClassifier(),
+            n_estimators=n_estimators,
+            max_samples=max_samples,
+            max_features=1.0,
+            bootstrap=False,
+            bootstrap_features=False,
+            n_jobs=1,
+            random_state=None,
+            verbose=verbose)
+
+class RandomPatchesCostSensitiveDecisionTreeClassifier(BaggingClassifier):
+    def __init__(self,
+                 n_estimators=100,
+                 max_samples=0.5,
+                 max_features=0.5,
+                 verbose=False):
+        super(BaggingClassifier, self).__init__(
+            base_estimator=CostSensitiveDecisionTreeClassifier(),
+            n_estimators=n_estimators,
+            max_samples=max_samples,
+            max_features=max_features,
+            bootstrap=True,
+            bootstrap_features=True,
+            n_jobs=1,
+            random_state=None,
+            verbose=verbose)
 
 
-(estimators, estimators_features) = f.estimators_, f.estimators_features_
+algos = [BaggingCostSensitiveDecisionTreeClassifier(verbose=2),
+         PastingCostSensitiveDecisionTreeClassifier(verbose=2),
+         RandomPatchesCostSensitiveDecisionTreeClassifier(verbose=2),
+         CostSensitiveDecisionTreeClassifier()]
 
-temp = zip(estimators, estimators_features)
-estimator, features = temp[0]
+predict = []
+for algo in algos:
+    print algo
+    algo.fit(X_train, y_train, cost_mat_train)
+    predict.append(algo.predict(X_test))
 
-temp = f.predict_proba(X_test)
+for i, pred in enumerate(predict):
+    print algos[i]
+    print savings_score(y_test, pred, cost_mat_test)
+
+for e in range(3):
+    for i, estimator in enumerate(algos[e].estimators_):
+        print algos[e].__class__, i, savings_score(y_test, estimator.predict(X_test), cost_mat_test)
 
 
-#
-# y_pred_test_rf = RandomForestClassifier(random_state=0).fit(X_train, y_train).predict(X_test)
-#
-# f = CostSensitiveDecisionTreeClassifier()
-# y_pred_test_csdt = f.fit(X_train, y_train, cost_mat_train).predict(X_test)
-#
-# # Savings using only RandomForest
-# print savings_score(y_test, y_pred_test_rf, cost_mat_test)
-#
-# # Savings using CSDecisionTree
-# print savings_score(y_test, y_pred_test_csdt, cost_mat_test)
-#
-#
-# def test_classification():
-#     """Check classification for various parameter settings."""
-#     rng = check_random_state(0)
-#     X_train, X_test, y_train, y_test = train_test_split(iris.data,
-#                                                         iris.target,
-#                                                         random_state=rng)
-#     grid = ParameterGrid({"max_samples": [0.5, 1.0],
-#                           "max_features": [1, 2, 4],
-#                           "bootstrap": [True, False],
-#                           "bootstrap_features": [True, False]})
-#
-# params = dict(base_estimator=None,
-#                  n_estimators=10,
-#                  max_samples=1.0,
-#                  max_features=1.0,
-#                  bootstrap=True,
-#                  bootstrap_features=False,
-#                  oob_score=False,
-#                  n_jobs=1,
-#                  random_state=None,
-#                  verbose=0)
-#
-#     for base_estimator in [None,
-#                            DummyClassifier(),
-#                            Perceptron(),
-#                            DecisionTreeClassifier(),
-#                            KNeighborsClassifier(),
-#                            SVC()]:
-#         for params in grid:
-#             BaggingClassifier(base_estimator=base_estimator,
-#                               random_state=rng,
-#                               **params).fit(X_train, y_train).predict(X_test)
+check https://github.com/scikit-learn/scikit-learn/blob/9c8f6561874ae6ba45136ddf41c54a63ce62c616/sklearn/tree/_tree.pyx#L1079
