59be1e311562e1a897acf1fb3fde444bb57e60aa
Al.Bahnsen <al.bahnsen@gmail.com>
2014-08-17 17:47:07 +0200

    fix merge

--- costcla/models/__init__.py
+++ costcla/models/__init__.py
@@ -1,8 +1,3 @@
 from directcost import BayesMinimumRiskClassifier
-<<<<<<< a95b5a786d5e264bc31c0f40d2ac57a10fbc368b
 from cost_tree import CostSensitiveDecisionTreeClassifier
-
-
-=======
 from regression import CostSensitiveLogisticRegression
->>>>>>> 5ee05ff9be567d7e0d4624cb15e29aeee509f765
