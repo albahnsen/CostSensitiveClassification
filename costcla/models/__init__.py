b86cb4c41971c9ab1bbb83bd54d1e234b2e251c0
Al.Bahnsen <al.bahnsen@gmail.com>
2014-08-17 20:01:11 +0200

    fix order to load

--- costcla/models/__init__.py
+++ costcla/models/__init__.py
@@ -1,8 +1,10 @@
 from directcost import BayesMinimumRiskClassifier
 from cost_tree import CostSensitiveDecisionTreeClassifier
+from regression import CostSensitiveLogisticRegression
+
 from cost_ensemble import PastingCostSensitiveDecisionTreeClassifier
 from cost_ensemble import RandomPatchesCostSensitiveDecisionTreeClassifier
 from cost_ensemble import BaggingCostSensitiveDecisionTreeClassifier
 from cost_ensemble import CostSensitiveRandomForestClassifier
 
-from regression import CostSensitiveLogisticRegression
+
