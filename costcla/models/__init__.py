4d75ba4cbebae9cbdbe773f98eba99cc481c6df9
Al.Bahnsen <al.bahnsen@gmail.com>
2014-08-17 11:29:04 +0200

    add logistic_cost_loss

--- costcla/models/__init__.py
+++ costcla/models/__init__.py
@@ -1,7 +1,11 @@
 from directcost import BayesMinimumRiskClassifier
+<<<<<<< 8f747caecad1042376c272d82da6654d5a798a15
 from cost_tree import CostSensitiveDecisionTreeClassifier
 from cost_ensemble import PastingCostSensitiveDecisionTreeClassifier
 from cost_ensemble import RandomPatchesCostSensitiveDecisionTreeClassifier
 from cost_ensemble import BaggingCostSensitiveDecisionTreeClassifier
 from cost_ensemble import CostSensitiveRandomForestClassifier
 
+=======
+from regression import CostSensitiveLogisticRegression
+>>>>>>> 5ee05ff9be567d7e0d4624cb15e29aeee509f765
