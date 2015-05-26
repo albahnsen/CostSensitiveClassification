a28fbc8cfca8be485141dbba5bd99616a0bdfc80
AlBahnsen <al.bahnsen@gmail.com>
2015-05-26 11:24:22 +0200

    new version

--- costcla/__init__.py
+++ costcla/__init__.py
@@ -24,7 +24,7 @@ You can install ``costcla`` with ``pip``::
 # Authors: Alejandro Correa Bahnsen <al.bahnsen@gmail.com>
 # License: BSD 3 clause
 
-__version__ = '0.04.dev2'
+__version__ = '0.4'
 
 from metrics import *
 from datasets import *
