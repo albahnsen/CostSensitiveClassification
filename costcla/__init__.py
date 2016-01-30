01ce0433a8e47952b282e921426d2d5cb28c34ea
AlBahnsen <al.bahnsen@gmail.com>
2016-01-30 10:28:30 -0500

    update version to 0.6dev

--- costcla/__init__.py
+++ costcla/__init__.py
@@ -24,7 +24,7 @@ You can install ``costcla`` with ``pip``::
 # Authors: Alejandro Correa Bahnsen <al.bahnsen@gmail.com>
 # License: BSD 3 clause
 
-__version__ = '0.5'
+__version__ = '0.6.dev'
 
 from .metrics import *
 from .datasets import *
